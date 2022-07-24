1f6c6cc20098a13dfbb01fe621a1cf07be73cebd
dgopikumar <gopi7dhoni@gmail.com>
2022-07-24 22:49:23 +0530

    some more validatons are added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 const GOERLI_URL = process.env.GOERLI_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
+const GOERLI_API_KEY = process.env.GOERLI_API_KEY;
+// Latest contract deployed into Goerli network (refer Address) : 0x938723DcE2178403ee25b286480509E690Ae52a8
 module.exports = {
   solidity: {
     version: "0.8.4"
@@ -16,6 +18,11 @@ module.exports = {
     goerli: {
       url: GOERLI_URL,
       accounts:[PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      goerli: GOERLI_API_KEY
     }
   }
 };
--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -20,7 +20,7 @@ async function printMemos(memos) {
 
 async function main() {
   //Get example accounts
-  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+  const [owner, tipper, tipper2, tipper3, tipper4, nextOwner] = await hre.ethers.getSigners();
 
   //Get the contract to deploy & deploy contract
   const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
@@ -29,21 +29,26 @@ async function main() {
   console.log("BuyMeACoffee deployed to: ", buyMeACoffee.address);
 
   //Check balances before the coffee purchase
-  const addresses = [owner.address, tipper.address, buyMeACoffee.address];
-  console.log("== start ==");
+  const addresses = [owner.address, nextOwner.address, tipper.address, buyMeACoffee.address];
+  console.log("== start... ==");
   await printBalances(addresses);
-
+  
   //Buy the owner a few coffees
   const tip = {value: hre.ethers.utils.parseEther("1")};
-  await buyMeACoffee.connect(tipper).buyCoffee("Carolina", "You're the best!", tip);
-  await buyMeACoffee.connect(tipper2).buyCoffee("Vitto", "Amazing teacher :)", tip);
-  await buyMeACoffee.connect(tipper3).buyCoffee("Kay", "I love my proof of knowledge NFT", tip);
+  const tip2 = {value: hre.ethers.utils.parseEther("2")};
+  const tip3 = {value: hre.ethers.utils.parseEther("3")};
+  const tip4 = {value: hre.ethers.utils.parseEther("4")};
+  await buyMeACoffee.connect(tipper).buyCoffee("Carolina", "Enjoy your Black Coffee!", tip);
+  await buyMeACoffee.connect(tipper2).buyCoffee("Vitto", "Enjoy your Espresso Coffee!", tip2);
+  await buyMeACoffee.connect(tipper3).buyCoffee("Kay", "Enjoy your Cappuccino Coffee!", tip3);
+  await buyMeACoffee.connect(tipper4).buyCoffee("Gopi", "Enjoy your Macchiato Coffee!", tip4);
 
   //Check balances after coffee purchase
   console.log("=== bought coffee ===");
   await printBalances(addresses);
 
   //Withdraw funds
+  await buyMeACoffee.connect(owner).changeOwner(nextOwner.address);
   await buyMeACoffee.connect(owner).withdrawTips();
 
   //Check balance after withdraw
